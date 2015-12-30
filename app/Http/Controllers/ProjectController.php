e54b92e792cbdd761e83a3d00e6c6044ca1a27da
Stefano Groenland <stefano.groenland@gmail.com>
2015-12-30 09:38:38 +0100

    #20 fixed, addProject() wordt niet meer altijd opgeslagen in de DB,

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -36,85 +36,82 @@ class ProjectController extends Controller
         }elseif($request['bedrijf'] == 'moodles') {
             $request->session()->flash('alert-danger', 'Klantaccount mag geen \'moodles\' bevatten als bedrijfsnaam.');
             return redirect('/newproject');
-        }
-
-        if(isset($_POST['radmaak'])){
-            Validator::make($request->all(),[
+        }else {
+            if (isset($_POST['radmaak'])) {
+                Validator::make($request->all(), [
 //                projecten table
-                'titel'                     => 'required|max:255|unique:projecten',
-                'status'                    => 'required',
-                'prioriteit'                => 'required',
-                'soort'                     => 'required',
-                'projectnaam'               => 'required',
-                'projecturl'                => 'required',
-                'gebruikersnaam'            => 'required',
-                'wachtwoord'                => 'required',
-                'omschrijvingproject'       => 'required',
-                'gebruiker_id'              => 'required',
+                    'titel' => 'required|max:255|unique:projecten',
+                    'status' => 'required',
+                    'prioriteit' => 'required',
+                    'soort' => 'required',
+                    'projectnaam' => 'required',
+                    'projecturl' => 'required',
+                    'gebruikersnaam' => 'required',
+                    'wachtwoord' => 'required',
+                    'omschrijvingproject' => 'required',
+                    'gebruiker_id' => 'required',
 
 //                gebruikers table
-                'username'                  => $request['username'],
-                'password'                  => bcrypt($request['password']),
-                'email'                     => $request['email'],
-                'bedrijf'                   => $request['bedrijf'],
-                'voornaam'                  => $request['voornaam'],
-                'tussenvoegsel'             => $request['tussenvoegsel'],
-                'achternaam'                => $request['achternaam'],
-                'geslacht'                  => $request['geslacht'],
-                'telefoonnummer'            => $request['telefoonnummer'],
-            ]);
-            $user = User::create([
-                'username'                  => $request['username'],
-                'password'                  => bcrypt($request['password']),
-                'email'                     => $request['email'],
-                'bedrijf'                   => $request['bedrijf'],
-                'voornaam'                  => $request['voornaam'],
-                'tussenvoegsel'             => $request['tussenvoegsel'],
-                'achternaam'                => $request['achternaam'],
-                'geslacht'                  => $request['geslacht'],
-                'telefoonnummer'            => $request['telefoonnummer'],
+                    'username' => $request['username'],
+                    'password' => bcrypt($request['password']),
+                    'email' => $request['email'],
+                    'bedrijf' => $request['bedrijf'],
+                    'voornaam' => $request['voornaam'],
+                    'tussenvoegsel' => $request['tussenvoegsel'],
+                    'achternaam' => $request['achternaam'],
+                    'geslacht' => $request['geslacht'],
+                    'telefoonnummer' => $request['telefoonnummer'],
                 ]);
-            Project::create([
-                'titel'                     => $request['titel'],
-                'status'                    => $request['status'],
-                'prioriteit'                => $request['prioriteit'],
-                'soort'                     => $request['soort'],
-                'projectnaam'               => $request['projectnaam'],
-                'projecturl'                => $request['projecturl'],
-                'gebruikersnaam'            => $request['gebruikersnaam'],
-                'wachtwoord'                => bcrypt($request['wachtwoord']),
-                'omschrijvingproject'       => $request['omschrijvingproject'],
-                'gebruiker_id'              => $user->id,
-            ]);
-            }
-            elseif(isset($_POST['radkoppel'])){
-                Validator::make($request->all(),[
-                    'titel'                 => 'required|max:255|unique:projecten',
-                    'status'                => 'required',
-                    'prioriteit'            => 'required',
-                    'soort'                 => 'required',
-                    'projectnaam'           => 'required',
-                    'projecturl'            => 'required',
-                    'gebruikersnaam'        => 'required',
-                    'wachtwoord'            => 'required',
-                    'omschrijvingproject'   => 'required',
-                    'gebruiker_id'          => 'required',
-                    ]);
+                $user = User::create([
+                    'username' => $request['username'],
+                    'password' => bcrypt($request['password']),
+                    'email' => $request['email'],
+                    'bedrijf' => $request['bedrijf'],
+                    'voornaam' => $request['voornaam'],
+                    'tussenvoegsel' => $request['tussenvoegsel'],
+                    'achternaam' => $request['achternaam'],
+                    'geslacht' => $request['geslacht'],
+                    'telefoonnummer' => $request['telefoonnummer'],
+                ]);
                 Project::create([
-                    'titel'                 => $request['titel'],
-                    'status'                => $request['status'],
-                    'prioriteit'            => $request['prioriteit'],
-                    'soort'                 => $request['soort'],
-                    'projectnaam'           => $request['projectnaam'],
-                    'projecturl'            => $request['projecturl'],
-                    'gebruikersnaam'        => $request['gebruikersnaam'],
-                    'wachtwoord'            => bcrypt($request['wachtwoord']),
-                    'omschrijvingproject'   => $request['omschrijvingproject'],
-                    'gebruiker_id'          => $request['gebruiker_id'],
+                    'titel' => $request['titel'],
+                    'status' => $request['status'],
+                    'prioriteit' => $request['prioriteit'],
+                    'soort' => $request['soort'],
+                    'projectnaam' => $request['projectnaam'],
+                    'projecturl' => $request['projecturl'],
+                    'gebruikersnaam' => $request['gebruikersnaam'],
+                    'wachtwoord' => bcrypt($request['wachtwoord']),
+                    'omschrijvingproject' => $request['omschrijvingproject'],
+                    'gebruiker_id' => $user->id,
+                ]);
+            } elseif (isset($_POST['radkoppel'])) {
+                Validator::make($request->all(), [
+                    'titel' => 'required|max:255|unique:projecten',
+                    'status' => 'required',
+                    'prioriteit' => 'required',
+                    'soort' => 'required',
+                    'projectnaam' => 'required',
+                    'projecturl' => 'required',
+                    'gebruikersnaam' => 'required',
+                    'wachtwoord' => 'required',
+                    'omschrijvingproject' => 'required',
+                    'gebruiker_id' => 'required',
+                ]);
+                Project::create([
+                    'titel' => $request['titel'],
+                    'status' => $request['status'],
+                    'prioriteit' => $request['prioriteit'],
+                    'soort' => $request['soort'],
+                    'projectnaam' => $request['projectnaam'],
+                    'projecturl' => $request['projecturl'],
+                    'gebruikersnaam' => $request['gebruikersnaam'],
+                    'wachtwoord' => bcrypt($request['wachtwoord']),
+                    'omschrijvingproject' => $request['omschrijvingproject'],
+                    'gebruiker_id' => $request['gebruiker_id'],
                 ]);
             }
-
-
+        }
         $request->session()->flash('alert-success', 'Project toegevoegd.');
         return redirect('/newproject');
     }
