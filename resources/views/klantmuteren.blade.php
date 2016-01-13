6d6778d57045da44226a4d569ee21a6f7bb3906a
Stefano Groenland <stefano.groenland@gmail.com>
2016-01-13 11:29:19 +0100

    moodles favicon wordt op elke pagina goed aangeroepen.

--- resources/views/klantmuteren.blade.php
+++ resources/views/klantmuteren.blade.php
@@ -11,7 +11,7 @@
 
     <title>Moodles - Helpdesk</title>
 
-    <link rel="shortcut icon" type="image/ico" href="./favicon.ico" />
+    <link rel="shortcut icon" type="image/ico" href="../../favicon.ico" />
 </head>
 
 
