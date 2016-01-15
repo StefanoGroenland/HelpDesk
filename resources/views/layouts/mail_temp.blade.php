cac117e18e6264b3b22af48c2c5de621375eb238
Stefano Groenland <stefano.groenland@gmail.com>
2016-01-15 12:43:34 +0100

    2/2 Final touches implemented, email van helpdesk@moodles.nl wordt nu gebruikt en echte klant email.

--- resources/views/layouts/mail_temp.blade.php
+++ resources/views/layouts/mail_temp.blade.php
@@ -3,28 +3,31 @@
 </head>
 <body style="background-color:#f2f2f2;!important;" >
 {{--begin container--}}
-<div class="container panel panel-default">
+<div class="container">
+    <div class="panel panel-default">
+
     <div class="panel-body">
 
-{{--begin row--}}
-    <div class="row">
-        <div class="col-sm-2 col-md-2 col-lg-2"></div>
-        <div class="col-sm-8 col-md-8 col-lg-8">
-            <br>
-            <img src="http://helpdesk.moodles.nl/assets/images/logo.png" class="text-center center-block img-responsive"
-            width="200px" height="200px">
-            <h3 class="text-center page-header" style="border-bottom: none;!important;" >
-                @yield('onderwerp')
-            </h3>
-                @yield('bericht')
+        {{--begin row--}}
+        <div class="row">
+            <div class="col-sm-2 col-md-2 col-lg-2"></div>
+            <div class="col-sm-8 col-md-8 col-lg-8">
+                <br>
+                <img src="http://helpdesk.moodles.nl/assets/images/logo.png" class="text-center center-block img-responsive"
+                width="200px" height="200px">
+                <h3 class="text-center page-header" style="border-bottom: none;!important;" >
+                    @yield('onderwerp')
+                </h3>
+                    @yield('bericht')
 
-                @yield('footer')
+                    @yield('footer')
+            </div>
+            <div class="col-sm-2 col-md-2 col-lg-2"></div>
         </div>
-        <div class="col-sm-2 col-md-2 col-lg-2"></div>
-    </div>
     {{--endrow--}}
-
     </div>
+</div>
+
 
 
 
