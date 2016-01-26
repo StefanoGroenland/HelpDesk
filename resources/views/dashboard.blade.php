a51158246e2735360c618a8728978ebb5c4d44ad
Stefano Groenland <stefano.groenland@gmail.com>
2016-01-26 15:47:30 +0100

    #responsiveness added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,7 +23,7 @@
                 </div>
                        @include('layouts.projectendashboard')
                 </div>
-                <div class="col-lg-12">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                        @include('layouts.feedbacktable')
               </div>
             </div>
