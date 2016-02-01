d09996b39d21e71bf213002570c035e6ecb5ae48
Stefano Groenland <stefano.groenland@gmail.com>
2016-02-01 08:39:11 +0100

    isAdmin() middleware aangepast naar nieuwe manier van admin check.

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,7 +16,7 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::user()->bedrijf != 'moodles'){
+        if(Auth::user()->rol != 'medewerker'){
             return redirect('/dashboard');
         }
         return $next($request);
